e50f724a634232579d5c5babaeb3e7daa2b3099c
omar-diane <omar.diane@laplateforme.io>
2021-11-02 12:51:14 +0100

    Etape Job 01 terminer.

---
+++ jour01/job01/index.php
@@ -0,0 +1,14 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo $str2."". $str ."". $str3;
+$val = 6;
+echo $val;
+$val2 = 4;
+echo $val2;
+$myBool = true;
+echo$myBool;
+$myBool2 = false;
+echo $myBool2; 
+?>
